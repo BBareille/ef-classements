38f1ddde84926b5bc7372025d59b9d41959ee5b8
Baptiste <baptiste.bareille@outlook.com>
2023-04-20 12:03:50 +0200

    rank-faction V1 - done

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,13 +1,26 @@
 @extends('layouts.app')
 
-@section('title', 'Plugin home')
+@section('title', 'Classement Faction')
 
 @section('content')
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.2/font/bootstrap-icons.css">
+    <link rel="stylesheet" href="https://unpkg.com/bootstrap-table@1.21.4/dist/bootstrap-table.min.css">
+
 
     @if($factionList)
-        <table class="table table-bordered">
+        <table id="table"
+               data-toggle="table"
+               data-search="true"
+               data-pagination="true"
+               data-page-size="4"
+               data-pagination-pre-text="Précédent"
+               data-pagination-next-text="Suivant"
+               data-pagination-detail="false"
+               data-pagination-parts="pageList"
+        >
             <thead>
-            <th>Pos</th>
+            <th data-field="position" data-sortable="true" data-width="20">Pos</th>
             <th>Nom de faction</th>
             <th>Nombres de points</th>
             </thead>
@@ -30,6 +43,10 @@
             @endforeach
             </tbody>
         </table>
+
+        <script src="https://cdn.jsdelivr.net/npm/jquery/dist/jquery.min.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+        <script src="https://unpkg.com/bootstrap-table@1.21.4/dist/bootstrap-table.min.js"></script>
 
     @endif
 @endsection
